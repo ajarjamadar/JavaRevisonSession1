63d055fd44647ce48bcf90d9314cf458b027b48c
Azaruddin Jamadar <arjamadar@gmail.com>
2023-05-21 12:16:55 +0530

    [ajar]:multiplication functionality added in sample class.

--- src/sample.java
+++ src/sample.java
@@ -14,11 +14,20 @@ public class sample {
 		int c=a+b;
 		System.out.println("addition of a+b = "+c);
 	}
+	
+	public static void mul()
+	{
+		int d=20;
+		sample sample = new sample();
+		int c = sample.a*d;
+		System.out.println("multiplication of a*d = "+c);
+	}
 
 	public static void main(String[] args) {
 	
 		sample s1 = new sample();
 		s1.add();
+		s1.mul();
 		
 
 	}
